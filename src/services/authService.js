29a91bea64c5365ffddae6f7f0e212dd03ab68be
saivighnesh2190 <2300030455@kluniversity.in>
2025-09-13 07:50:29 +0530

    first commit

---
+++ src/services/authService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_URL = "ec2-18-234-131-134.compute-1.amazonaws.com/auth";
+
+export const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  localStorage.setItem("token", response.data);
+  return response.data;
+};
+
+export const signup = async (username, email, password) => {
+  return axios.post(`${API_URL}/signup`, { username, email, password });
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
